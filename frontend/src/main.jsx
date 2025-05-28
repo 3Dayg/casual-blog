73fa173b78e32246565fab79af848a95d7583bc8
3Daygg_ <alvarez.matias.n@gmail.com>
2025-05-28 18:55:10 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, basepath: '/casual-blog' })
 
 // Render the app
 const rootElement = document.getElementById('root')
